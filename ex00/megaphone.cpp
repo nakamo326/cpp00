60c193a116824100a6d63db626f88fda7838f36c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-07-08 23:41:35 +0900

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else if (argc >= 2)
+    {
+        for (int i = 1; i < argc; i++)
+            for (int j = 0; argv[i][j] != '\0'; j++)
+                std::cout << char(std::toupper(argv[i][j]));
+        std::cout << std::endl;
+    }
+    return 0;
+}
